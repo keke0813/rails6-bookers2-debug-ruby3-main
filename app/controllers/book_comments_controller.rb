3f7e83057d6d6d64f21419092acf36170702fb2c
keke0813 <149664866+keke0813@users.noreply.github.com>
2024-01-04 14:09:20 +0000

    応用課題６ Finish

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,20 +1,24 @@
-  class BookCommentsController < ApplicationController
-    def create
-      book = Book.find(params[:book_id])
-      @comment = current_user.book_comments.new(book_comment_params)
-      @comment.book_id = book.id
-      @comment.save
+class BookCommentsController < ApplicationController
+
+  def create
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    @user = @book.user
     #redirect_to request.referer
-    end
-
-    def destroy
-      BookComment.find(params[:id]).destroy
-     #redirect_to request.referer
-    end
-
-    private
-
-    def book_comment_params
-      params.require(:book_comment).permit(:comment)
-    end
   end
+
+  def destroy
+    @comment = BookComment.find(params[:id])
+    BookComment.find(params[:id]).destroy
+    #redirect_to request.referer
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+
+end
